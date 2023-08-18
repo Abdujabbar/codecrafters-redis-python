ca958654b8d9b8115444964a77fb3e2ee4b02f2a
Abdujabbar MIRKHALIKOV <abdujabbar.mirkhalikov@prestataire.sewan.fr>
2023-08-18 16:43:21 +0500

    Added ping pong

--- app/main.py
+++ app/main.py
@@ -6,7 +6,13 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept()
+    conn, address = server_socket.accept()
 
+    while conn:
+        data = str(conn.recv(1024))
+
+        if data == '+PING\n\r':
+            conn.send('+PONG\n\r'.encode())
+    
 if __name__ == "__main__":
     main()
