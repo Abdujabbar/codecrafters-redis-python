badeea7c690612914a28267697add916427946ab
Abdujabbar MIRKHALIKOV <abdujabbar.mirkhalikov@prestataire.sewan.fr>
2023-08-21 16:50:44 +0500

    Multiple ping-pong cmd

--- app/main.py
+++ app/main.py
@@ -6,13 +6,18 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    conn, address = server_socket.accept()
 
-    while conn:
-        data = conn.recv(1024).decode()
+    while True:
+        conn, address = server_socket.accept()
 
-        if "ping" in data.lower():
-            conn.send("$4\r\nPONG\r\n".encode())
+        while conn:
+            data = conn.recv(1024).decode()
+            
+            if not data:
+                break
+            
+            if "ping" in data.lower():
+                conn.send("$4\r\nPONG\r\n".encode())
     
 if __name__ == "__main__":
     main()
