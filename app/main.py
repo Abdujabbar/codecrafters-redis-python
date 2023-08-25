d0cc86e72cda1257a5d16180210e3648768f6caa
Abdujabbar MIRKHALIKOV <abdujabbar.mirkhalikov@prestataire.sewan.fr>
2023-08-26 01:52:57 +0500

    Modify the logic

--- app/main.py
+++ app/main.py
@@ -13,65 +13,69 @@ class StorageSet(dict):
         return cls._instance
 
 
-def execute_process(server_socket, storage):
-    conn, address = server_socket.accept()
-    
-    while conn:
-        data = conn.recv(1024).decode()
-        partials = list(filter(None, data.split("\r\n")))
-        
-        if not partials:
-            return
-        
-        _, _, command, *args = partials
-        command = command.lower()
-        
-        if command == 'ping':
-            conn.send("$4\r\nPONG\r\n".encode())
-        elif command == 'echo':
-            partials.append('')
-            conn.send('\r\n'.join(partials[3:]).encode())
-        elif command == 'set':
-            partials = partials[3:]
-            expire = 10 ** 30
-            if len(partials) > 4:
-                expire = int(partials[-1]) / 1000
-            key = partials[1]
-            value = partials[3]
-            storage[key] = (value, expire, time.time())
-            conn.send("$2\r\nOK\r\n".encode())
-        elif command == 'get':
-            partials = partials[3:]
-            key = partials[-1]
-            if key in storage:
-                value, expire, prev_time = storage.get(key)
-                curr_time = time.time()
 
-                if curr_time - prev_time > expire:
+
+class RedisServer:
+    def __init__(self, host, port, storage) -> None:
+        self.host = host
+        self.port = port
+        self.storage = storage
+
+    def run(self):
+        server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+        for _ in range(20):
+            x = threading.Thread(target=self.execute_process, args=(server_socket, ))
+            x.start()
+
+
+    def execute_process(self, server_socket):
+        conn, _ = server_socket.accept()
+        while conn:
+            data = conn.recv(1024).decode()
+            partials = list(filter(None, data.split("\r\n")))
+            
+            if not partials:
+                return
+            
+            _, _, command, *args = partials
+            command = command.lower()
+            
+            if command == 'ping':
+                conn.send("$4\r\nPONG\r\n".encode())
+            elif command == 'echo':
+                partials.append('')
+                conn.send('\r\n'.join(partials[3:]).encode())
+            elif command == 'set':
+                partials = partials[3:]
+                expire = 10 ** 30
+                if len(partials) > 4:
+                    expire = int(partials[-1]) / 1000
+                key = partials[1]
+                value = partials[3]
+                self.storage[key] = (value, expire, time.time())
+                conn.send("$2\r\nOK\r\n".encode())
+            elif command == 'get':
+                partials = partials[3:]
+                key = partials[-1]
+                if key in self.storage:
+                    value, expire, prev_time = self.storage.get(key)
+                    curr_time = time.time()
+
+                    if curr_time - prev_time > expire:
+                        conn.send(f"$-1\r\n".encode())
+                    
+                    conn.send(f'${len(value)}\r\n{value}\r\n'.encode())
+                else:
                     conn.send(f"$-1\r\n".encode())
-                
-                conn.send(f'${len(value)}\r\n{value}\r\n'.encode())
             else:
-                conn.send(f"$-1\r\n".encode())
-        else:
-            raise Exception('Not Implemented')
-    
+                raise Exception('Not Implemented')
+
+
     
 def main():
     print("Logs from your program will appear here!")
-
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    storage = StorageSet()
-    threads = []
-
-    for _ in range(20):
-        x = threading.Thread(target=execute_process, args=(server_socket, storage,))
-        threads.append(x)
-        x.start()
-
-    for _, thread in enumerate(threads):
-        thread.join()
-
+    RedisServer("localhost", 6379, StorageSet()).run()
+    
 
 if __name__ == "__main__":
     main()
